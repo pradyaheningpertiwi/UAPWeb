a27a8a16aaa1042227a4de3f479ef693478f7f36
yusufnatann <152268898+yusufnatann@users.noreply.github.com>
2024-05-28 15:49:38 +0700

    Update login.php

--- login.php
+++ login.php
@@ -7,18 +7,17 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
-<div class="container">
-    <h1>Event Organizer</h1>
-
-    <h2>Login</h2>
-    <form id="loginForm" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        <input type="email" id="email" name="email" placeholder="Email" required>
-        <input type="password" id="password" name="password" placeholder="Password" required>
-        <button type="submit" name="login">Login</button>
-    </form>
-    <a style href="index.php">Register</a>
-</div>
-
+    <div class="header">
+        <h1>Event Organizer</h1>
+        <h2>Login</h2>
+    </div>
+    <div class="container">
+        <form id="loginForm" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <input type="email" id="email" name="email" placeholder="Email" required>
+            <input type="password" id="password" name="password" placeholder="Password" required>
+            <button type="submit" name="login">Login</button>
+        </form>
+        <a href="index.php">Register</a>
+    </div>
 </body>
 </html>
